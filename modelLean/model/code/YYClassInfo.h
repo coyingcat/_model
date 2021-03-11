02c452797666ad7ec309fb3631a38a9739e4a4c5
邓江洲 <dengjiangzhouReal@gmail.com>
2021-03-11 15:48:36 +0800

    @class YYClassIvarInfo;

--- modelLean/model/code/YYClassInfo.h
+++ modelLean/model/code/YYClassInfo.h
@@ -79,27 +79,8 @@ typedef NS_OPTIONS(NSUInteger, YYEncodingType) {
 YYEncodingType YYEncodingGetType(const char *typeEncoding);
 
 
-/**
- Instance variable information.
- */
-@interface YYClassIvarInfo : NSObject
 
-
-@property (nonatomic, assign, readonly) Ivar ivar;              ///< ivar opaque struct
-@property (nonatomic, strong, readonly) NSString *name;         ///< Ivar's name
-@property (nonatomic, assign, readonly) ptrdiff_t offset;       ///< Ivar's offset
-@property (nonatomic, strong, readonly) NSString *typeEncoding; ///< Ivar's type encoding
-@property (nonatomic, assign, readonly) YYEncodingType type;    ///< Ivar's type
-
-/**
- Creates and returns an ivar info object.
- 
- @param ivar ivar opaque struct
- @return A new object, or nil if an error occurs.
- */
-- (instancetype)initWithIvar:(Ivar)ivar;
-@end
-
+@class YYClassIvarInfo;
 
 /**
  Method information.
--- modelLean/model/code/YYClassInfo.m
+++ modelLean/model/code/YYClassInfo.m
@@ -90,6 +90,35 @@ YYEncodingType YYEncodingGetType(const char *typeEncoding) {
     }
 }
 
+
+
+/**
+ Instance variable information.
+ */
+@interface YYClassIvarInfo : NSObject
+
+
+@property (nonatomic, assign, readonly) Ivar ivar;              ///< ivar opaque struct
+@property (nonatomic, strong, readonly) NSString *name;         ///< Ivar's name
+@property (nonatomic, assign, readonly) ptrdiff_t offset;       ///< Ivar's offset
+@property (nonatomic, strong, readonly) NSString *typeEncoding; ///< Ivar's type encoding
+@property (nonatomic, assign, readonly) YYEncodingType type;    ///< Ivar's type
+
+/**
+ Creates and returns an ivar info object.
+ 
+ @param ivar ivar opaque struct
+ @return A new object, or nil if an error occurs.
+ */
+- (instancetype)initWithIvar:(Ivar)ivar;
+@end
+
+
+
+
+
+
+
 @implementation YYClassIvarInfo
 
 - (instancetype)initWithIvar:(Ivar)ivar {
