966cf38e26ba777eb2e86b432634935a2eefed1d
邓江洲 <dengjiangzhouReal@gmail.com>
2021-03-12 11:34:20 +0800

    opt code

--- lean/model/code/YYModel.h
+++ lean/model/code/YYModel.h
@@ -11,20 +11,8 @@
 
 #import <Foundation/Foundation.h>
 
-#if __has_include(<YYModel/YYModel.h>)
-FOUNDATION_EXPORT double YYModelVersionNumber;
-FOUNDATION_EXPORT const unsigned char YYModelVersionString[];
-#import <YYModel/NSObject+YYModel.h>
-#import <YYModel/YYClassInfo.h>
-#else
-
-
 
 
 #import "Object+YYModel.h"
 #import "NSObject+YYModel.h"
 #import "YYClassInfo.h"
-
-
-
-#endif
